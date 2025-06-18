be3e29c81d8cd660034bdade7f18b189199e22bf
Aragor85 <djameltelecom@gmail.com>
2025-06-19 00:22:56 +0200

    Azure avec fastAPI 19 06 2025 0022

--- app/.ipynb_checkpoints/main-checkpoint.py
+++ app/.ipynb_checkpoints/main-checkpoint.py
@@ -1,16 +1,36 @@
 # app/main.py
 from fastapi import FastAPI
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from app.model_utils import load_model, predict_sentiment
+from typing import Literal
+import logging
+from opencensus.ext.azure.log_exporter import AzureLogHandler
 
+# --- Logger Application Insights ---
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+logger.addHandler(
+    AzureLogHandler(
+        connection_string="InstrumentationKey=0fdd4361-7b38-476f-a2b5-eefc99fefbb0"
+    )
+)
+
+# --- Application FastAPI ---
 app = FastAPI()
 
-# Chargement modèle + encodeur USE
+# Charger modèle + Universal Sentence Encoder
 model, use_model = load_model()
 
+# --- Schémas d’entrée ---
 class TextInput(BaseModel):
-    text: str
+    text: str = Field(..., alias="tweet", description="Please insert your tweet about Air Paradis")
 
+class Feedback(BaseModel):
+    text: str
+    predicted_label: Literal["positif", "negatif"]
+    correct_label: Literal["positif", "negatif"]
+
+# --- Routes ---
 @app.get("/")
 def read_root():
     return {"message": "Bienvenue dans l'API d'analyse des sentiments (modèle USE)"}
@@ -19,3 +39,20 @@ def read_root():
 def predict(input: TextInput):
     prediction = predict_sentiment(model, use_model, input.text)
     return {"prediction": prediction}
+
+@app.post("/feedback")
+def feedback(feedback: Feedback):
+    logger.warning(
+        "⚠️ Feedback utilisateur : mauvaise prédiction du modèle.",
+        extra={
+            "custom_dimensions": {
+                "input_text": feedback.text,
+                "predicted_label": feedback.predicted_label,
+                "correct_label": feedback.correct_label,
+                "type": "model_misclassification"
+            }
+        }
+    )
+    return {
+        "message": "Merci pour votre retour. Votre signalement a été enregistré pour améliorer le modèle."
+    }
