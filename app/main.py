49dc01c9d888d21d2c587bf27eef9becdca40cd0
Aragor85 <djameltelecom@gmail.com>
2025-06-19 00:52:54 +0200

    Azure avec fastAPI 18 06 2025 2130

--- app/main.py
+++ app/main.py
@@ -1,36 +1,16 @@
 # app/main.py
 from fastapi import FastAPI
-from pydantic import BaseModel, Field
+from pydantic import BaseModel
 from app.model_utils import load_model, predict_sentiment
-from typing import Literal
-import logging
-from opencensus.ext.azure.log_exporter import AzureLogHandler
 
-# --- Logger Application Insights ---
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.INFO)
-logger.addHandler(
-    AzureLogHandler(
-        connection_string="InstrumentationKey=0fdd4361-7b38-476f-a2b5-eefc99fefbb0"
-    )
-)
-
-# --- Application FastAPI ---
 app = FastAPI()
 
-# Charger modèle + Universal Sentence Encoder
+# Chargement modèle + encodeur USE
 model, use_model = load_model()
 
-# --- Schémas d’entrée ---
 class TextInput(BaseModel):
     text: str
 
-class Feedback(BaseModel):
-    text: str
-    predicted_label: Literal["positif", "negatif"]
-    correct_label: Literal["positif", "negatif"]
-
-# --- Routes ---
 @app.get("/")
 def read_root():
     return {"message": "Bienvenue dans l'API d'analyse des sentiments (modèle USE)"}
@@ -39,20 +19,3 @@ def read_root():
 def predict(input: TextInput):
     prediction = predict_sentiment(model, use_model, input.text)
     return {"prediction": prediction}
-
-@app.post("/feedback")
-def feedback(feedback: Feedback):
-    logger.warning(
-        "⚠️ Feedback utilisateur : mauvaise prédiction du modèle.",
-        extra={
-            "custom_dimensions": {
-                "input_text": feedback.text,
-                "predicted_label": feedback.predicted_label,
-                "correct_label": feedback.correct_label,
-                "type": "model_misclassification"
-            }
-        }
-    )
-    return {
-        "message": "Merci pour votre retour. Votre signalement a été enregistré pour améliorer le modèle."
-    }
